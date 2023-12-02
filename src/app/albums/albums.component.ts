9769c08f39d8b12b8f13908fca946d2edfdbb2cc
Rhetice YENOU <yrhetice@gmail.com>
2023-12-02 12:46:31 +0100

    Ajout du composant Albums

---
+++ src/app/albums/albums.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-albums',
+  templateUrl: './albums.component.html',
+  styleUrl: './albums.component.css'
+})
+export class AlbumsComponent {
+  title = "Page Principale Album Music"
+}
