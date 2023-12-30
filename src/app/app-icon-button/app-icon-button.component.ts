b5e152a1995eafbcc6cfc1d76d60d68533a0e970
yendev7 <yrhetice@gmail.com>
2023-12-30 11:23:04 +0100

    Creation de album detail component

---
+++ src/app/app-icon-button/app-icon-button.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-button',
+  templateUrl: './app-icon-button.component.html',
+  styleUrl: './app-icon-button.component.css'
+})
+export class AppIconButtonComponent {
+ @Input() title!: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlbumsComponent } from './albums/albums.component';
+import { AlbumDetailsComponent } from './album-details/album-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/Material/icon';
+import {  } from '@angular/Material/paginator';
+import { AppIconButtonComponent } from './app-icon-button/app-icon-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlbumsComponent
+    AlbumsComponent,
+    AlbumDetailsComponent,
+    AppIconButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
